cb927aace94fd498cb26feb40fcf812069b13c4a
Hilary <hilarykodji@gmail.com>
2022-03-19 02:25:41 +0000

    Read command line arg instead of requesting for input

--- echo/src/main.zig
+++ echo/src/main.zig
@@ -1,13 +1,21 @@
 const std = @import("std");
+const process = std.process;
 
 pub fn main() !void {
-    const stdin = std.io.getStdIn().reader();
+    // get stdout writer
     const stdout = std.io.getStdOut().writer();
 
-    var buffer: [1024]u8 = undefined;
-    const user_input: ?[]u8 = try stdin.readUntilDelimiterOrEof(&buffer, '\n');
-    
-    if (user_input) |input| {
-        try stdout.print("{s}\n", .{input});
-    }
+    var allocator: std.mem.Allocator = undefined;
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    allocator = arena.allocator();
+    var arg_it = try process.argsWithAllocator(allocator);
+
+    // Skip exe name
+    _ = arg_it.skip();  
+    // Get next arg or return empty string
+    const content = arg_it.next() orelse "";
+
+    try stdout.print("{s}\n", .{content});
 }
